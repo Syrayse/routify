4a6b635854f4865b0cd10c7f9f97c3a423bb7f88
syrayse <ruipedronetoreis12@gmail.com>
2020-06-05 18:14:24 +0000

    finished all algs

--- src/procura.pl
+++ src/procura.pl
@@ -22,12 +22,11 @@ vizinhos(S,P1,R,V,Q) :-
             nao(queue_member((A,_), Q))
             ), R).
 
-% ---- 1) Vizinhos em heap, calcula todos os vizinhos.
-vizinhos_heap2(S,P1,R,V,Ct) :-
+% ---- 0.1) Vizinhos em heap, calcula todos os vizinhos.
+vizinhos_heap(S,P1,R,V,Ct) :-
       findall( A,                          %(K,A,[(Pt,K,A)|P1]),
             (aresta(Pt, S, A,D),
-            ord_nonmember(A, V),
-            K is Ct + D
+            ord_nonmember(A, V)
             ), R1),
       list_to_ord_set(R1,R2),
       heap_aux(S,P1,R2,V,Ct,[],R).
@@ -36,8 +35,45 @@ heap_aux(S,P1,[],V,Ct,R,R).
 heap_aux(S,P1,[H|T],V,Ct,L,R) :-
       aresta(Pt,S,H,D),
       K is Ct + D,
-      heap_aux(S,P1,T,V,Ct,[(K,H,[(Pt,K,H)|P1])|L],R).
+      heap_aux(S,P1,T,V,Ct,[(K,K,H,[(Pt,K,H)|P1])|L],R).
 
+% ---- 0.2) Vizinhos, calcula todos de forma greedy.
+%vizinhos_greedy(S,P1,R,V,Dt,Y)
+vizinhos_greedy(S,P1,R,V,Ct,Dest) :-
+      findall( A,
+            (aresta(Pt, S, A,D),
+            ord_nonmember(A, V)
+            ), R1),
+      list_to_ord_set(R1,R2),
+      greedy_aux(S,P1,R2,V,Ct,[],R,Dest).
+
+greedy_aux(S,P1,[],V,Ct,R,R,Dest).
+greedy_aux(S,P1,[H|T],V,Ct,L,R,Dest) :-
+      aresta(Pt,S,H,D),
+      nodo(H,X1,Y1,_,_,_,_,_,_,_,_),
+      nodo(Dest,X2,Y2,_,_,_,_,_,_,_,_),
+      Dist is sqrt( (X1 - X2)*(X1 - X2) + (Y1 - Y2)*(Y1 - Y2) ),
+      Df is Ct + D,
+      greedy_aux(S,P1,T,V,Ct,[(Dist,Df,H,[(Pt,Df,H)|P1])|L],R,Dest).
+
+% ---- 0.2) Vizinhos, calcula todos de forma a estrela.
+vizinhos_star(S,P1,R,V,Ct,Dest) :-
+      findall( A,
+            (aresta(Pt, S, A,D),
+            ord_nonmember(A, V)
+            ), R1),
+      list_to_ord_set(R1,R2),
+      star_aux(S,P1,R2,V,Ct,[],R,Dest).
+
+star_aux(S,P1,[],V,Ct,R,R,Dest).
+star_aux(S,P1,[H|T],V,Ct,L,R,Dest) :-
+      aresta(Pt,S,H,D),
+      nodo(H,X1,Y1,_,_,_,_,_,_,_,_),
+      nodo(Dest,X2,Y2,_,_,_,_,_,_,_,_),
+      Dist is sqrt( (X1 - X2)*(X1 - X2) + (Y1 - Y2)*(Y1 - Y2) ),
+      Df is Ct + D,
+      K is Dist + Df,
+      star_aux(S,P1,T,V,Ct,[(K,Df,H,[(Pt,Df,H)|P1])|L],R,Dest).
 
 % ---- 1) Breadth-First.
 breadth_first(X, Y, P) :-
@@ -71,8 +107,8 @@ depth_first_aux(Y, Q, P,V) :-              % Pop da fila de espera
 
 % ---- 3) Uniform-Cost
 add_all_heap(Hf, [], Hf).
-add_all_heap(Hi, [(K,S,P)|T], Hf) :-
-      add_to_heap(Hi, K, (K,S,P), H),
+add_all_heap(Hi, [(K,Dt,S,P)|T], Hf) :-
+      add_to_heap(Hi, K, (K,Dt,S,P), H),
       add_all_heap(H, T, Hf).
 
 uniform_cost(X, Y, P) :-
@@ -81,14 +117,14 @@ uniform_cost(X, Y, P) :-
        fail.
 uniform_cost(X, Y, P) :-
       empty_heap(H),
-      add_to_heap(H, 0, (0,X,[(-1,0,X)]) , Heap),
+      add_to_heap(H, 0, (0,0,X,[(-1,0,X)]) , Heap),
       uniform_cost_aux( Y,Heap,R,[] ),
       inverso(R, P).
 
 uniform_cost_aux(Y, Heap, P,V) :-              % Se o prox elem for o destino, sai.
-      min_of_heap(Heap, C, (C,Y,P)).
+      min_of_heap(Heap, C, (C,_,Y,P)).
 uniform_cost_aux(Y, Heap, P,V) :-              % Pop da fila de espera
-      get_from_heap(Heap, K, (K,S,P1), Heap2), %append_queue([(S,P1)], Qp, Q),
+      get_from_heap(Heap, K, (K,Dt,S,P1), Heap2), %append_queue([(S,P1)], Qp, Q),
       ord_add_element(V,S,Vf),                  % Se nao foi visitado
       vizinhos_heap(S,P1,R,V,K),                    % Adiciona todos os vizinhos de S
       add_all_heap(Heap2, R, Heapf), !,             %append_queue(R, Qp, Qf),
@@ -97,10 +133,45 @@ uniform_cost_aux(Y, Heap, P,V) :-              % Pop da fila de espera
 % #########################################################
 % Metodos de Procura Informada.
 
-% ---- 1) Greedy Search.
+% ---- 1) Greedy Best-fist.
+greedy_best_first(X,Y,P) :-
+      (nao(nodo(X,_,_,_,_,_,_,_,_,_,_));
+       nao(nodo(Y,_,_,_,_,_,_,_,_,_,_))),!,
+       fail.
+greedy_best_first(X,Y,P) :-
+      empty_heap(H),
+      add_to_heap(H, 0, (0,0,X,[(-1,0,X)]) , Heap),
+      greedy_best_first_aux( Y,Heap,R,[] ),
+      inverso(R,P).
+
+greedy_best_first_aux(Y, Heap, P,V) :-              % Se o prox elem for o destino, sai.
+      min_of_heap(Heap, C, (C,_,Y,P)).
+greedy_best_first_aux(Y, Heap, P,V) :-              % Pop da fila de espera
+      get_from_heap(Heap, K, (K,Dt,S,P1), Heap2), %append_queue([(S,P1)], Qp, Q),
+      ord_add_element(V,S,Vf),                  % Se nao foi visitado
+      vizinhos_greedy(S,P1,R,V,Dt,Y),                    % Adiciona todos os vizinhos de S
+      add_all_heap(Heap2, R, Heapf), !,             %append_queue(R, Qp, Qf),
+      greedy_best_first_aux(Y,Heapf,P,Vf).
 
 % ---- 2) A-star.
+a_star(X,Y,P) :-
+      (nao(nodo(X,_,_,_,_,_,_,_,_,_,_));
+       nao(nodo(Y,_,_,_,_,_,_,_,_,_,_))),!,
+       fail.
+a_star(X,Y,P) :-
+      empty_heap(H),
+      add_to_heap(H, 0, (0,0,X,[(-1,0,X)]) , Heap),
+      a_star_aux( Y,Heap,R,[] ),
+      inverso(R,P).
 
+a_star_aux(Y, Heap, P,V) :-              % Se o prox elem for o destino, sai.
+      min_of_heap(Heap, C, (C,_,Y,P)).
+a_star_aux(Y, Heap, P,V) :-              % Pop da fila de espera
+      get_from_heap(Heap, K, (K,Dt,S,P1), Heap2), %append_queue([(S,P1)], Qp, Q),
+      ord_add_element(V,S,Vf),                  % Se nao foi visitado
+      vizinhos_star(S,P1,R,V,Dt,Y),                    % Adiciona todos os vizinhos de S
+      add_all_heap(Heap2, R, Heapf), !,             %append_queue(R, Qp, Qf),
+      a_star_aux(Y,Heapf,P,Vf).
 
 % #########################################################
 % Metodos de Incremental Cutout.
